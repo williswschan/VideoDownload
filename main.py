3d45d907911b2819f047926aa1baa2504c42b067
Willis Chan <willis.chan@hotmail.com.hk>
2024-12-20 16:32:19 +0800

    1.3

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+from flask import Flask
+
+app = Flask(__name__)
+access_count = 0
+
+@app.route('/')
+def home():
+    global access_count
+    access_count += 1
+    return f'Page accessed {access_count} times.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
